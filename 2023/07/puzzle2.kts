34a56c71788997c3d148f4960e1c9eb25904c468
Valentin Ilk <valentin.ilk@gmail.com>
2023-12-07 13:52:56 +0100

    Add day 7 (2023)

---
+++ 2023/07/puzzle2.kts
@@ -0,0 +1,39 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
+import java.io.File
+
+private data class Hand(val value: String, val bid: Int)
+
+private val cardValue = "J23456789TQKA"
+private val hexFormat = HexFormat {
+    upperCase = true
+    number.removeLeadingZeros = true
+}
+private var sum = 0
+
+File("input1.txt")
+    .readLines()
+    .map { line ->
+        val split = line.split(" ")
+        Hand(
+            value = split[0].toHandValue(),
+            bid = split[1].toInt(),
+        )
+    }
+    .sortedBy { it.value.hexToInt() }
+    .forEachIndexed { index, hand -> sum += (index + 1) * hand.bid }
+
+println("Sum: $sum")
+
+fun String.toHandValue(): String {
+    val charCount = HashMap<Char, Int>()
+    var value = ""
+    forEach {
+        charCount[it] = (charCount[it] ?: 0) + 1
+        value += cardValue.indexOf(it).toHexString(hexFormat)
+    }
+    val js = charCount.remove('J') ?: 0
+    val values = charCount.values.sortedDescending()
+    value = ((values.getOrNull(0) ?: 0) + js).toString() + (values.getOrNull(1) ?: 0).toString() + value
+    return value
+}
